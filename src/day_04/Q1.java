2b481214990f1a43bef705493effd16ae3723dc3
Murat DOGANCI <muratdoganci@Murats-MacBook-Pro.local>
2021-04-11 01:41:15 +0200

    Version2

---
+++ src/day_04/Q1.java
@@ -0,0 +1,11 @@
+package day_04;
+
+public class Q1 {
+
+	public static void main(String[] args) {
+		// Kullanıcının girdiği 4 basamaklı sayının ilk ve son rakamının toplamını
+		// ekrana yazdıran bir program yazınız.
+
+	}
+
+}
